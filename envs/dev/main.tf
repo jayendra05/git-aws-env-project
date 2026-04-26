d3d8104c9f6377af0b95ec7825e18e5bebea50ab
Jayendra <jayendra06mishra@gmail.com>
2026-04-26 21:16:26 +0530

    first commit

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,15 +2,32 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "vpc" {
+# VPC 1
+module "vpc1" {
   source     = "../../modules/vpc"
   cidr_block = "10.0.0.0/16"
-  vpc_name   = "dev-vpc"
+  vpc_name   = "dev-vpc-1"
 }
 
-module "subnet" {
+# Subnet for VPC 1
+module "subnet1" {
   source      = "../../modules/subnet"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.vpc1.vpc_id
   cidr_block  = "10.0.1.0/24"
-  subnet_name = "dev-subnet"
+  subnet_name = "dev-subnet-1"
+}
+
+# VPC 2
+module "vpc2" {
+  source     = "../../modules/vpc"
+  cidr_block = "10.1.0.0/16"
+  vpc_name   = "dev-vpc-2"
+}
+
+# Subnet for VPC 2
+module "subnet2" {
+  source      = "../../modules/subnet"
+  vpc_id      = module.vpc2.vpc_id
+  cidr_block  = "10.1.1.0/24"
+  subnet_name = "dev-subnet-2"
 }
