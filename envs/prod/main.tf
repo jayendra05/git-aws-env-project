8dc29400c608068d410e8a318d0a1a430c24c2ae
Jayendra <jayendra06mishra@gmail.com>
2026-04-29 04:06:49 +0530

    Adding Prod VPC

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -14,3 +14,11 @@ module "subnet1" {
   cidr_block  = "172.3.1.0/24"
   subnet_name = "prod-subnet-1"
 }
+
+
+module "subnet2" {
+  source      = "../../modules/subnet"
+  vpc_id      = module.vpc1.vpc_id
+  cidr_block  = "172.3.2.0/24"
+  subnet_name = "prod-subnet-2"
+}
