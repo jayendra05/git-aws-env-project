3f44bd1a33539b4a177c3ee51af559a7c2fc57ec
Jayendra <jayendra06mishra@gmail.com>
2026-04-29 19:17:40 +0530

    adding dynamodb for lock

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-jayendra"
-    key    = "dev/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "terraform-state-jayendra"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+
+    # Enable state locking
+    dynamodb_table = "terraform-locks"
+
+    # Recommended security settings
+    encrypt        = true
   }
 }
